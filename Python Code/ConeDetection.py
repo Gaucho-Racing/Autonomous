476fc5d1a37a14a2707a91b456c5f17ebb037de6
Aditya <avaswani@ucsb.edu>
2025-12-29 19:36:54 -0800

    Dataset Class PyTorch

--- Python Code/ConeDetection.py
+++ Python Code/ConeDetection.py
@@ -1,4 +1,24 @@
 import torch
 from PIL import Image
-from torchvision.datasets import CocoDetection
+from torchvision.datasets import CocoDetection, ImageFolder
 import torchvision.transforms as T
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+class ConeDataset():
+    def __init__(self, data_dir, transform=None):
+        self.data = ImageFolder(data_dir, transform=transform)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+    @property
+    def classes(self):
+        return self.data.classes
+
+#dataset = ConeDataset(data_dir=...)
